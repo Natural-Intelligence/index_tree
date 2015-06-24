5452f67ef4bdd91b8077c58e765f6423cc9cccbe
AlexStanovsky <alex.stanovsky@gmail.com>
2015-06-24 23:58:20 +0300

    fix coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,19 +1,9 @@
-require 'minitest/autorun'
-require 'minitest/benchmark'
-require 'active_record'
-require 'rails'
-require 'index_tree'
-
-require 'minitest/reporters'
-MiniTest::Reporters.use! [MiniTest::Reporters::DefaultReporter.new,
-                          MiniTest::Reporters::JUnitReporter.new]
-
 require 'simplecov'
 require 'simplecov-rcov'
 require 'coveralls'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-    SimpleCov::Formatter::RcovFormatter,
+   SimpleCov::Formatter::RcovFormatter,
     SimpleCov::Formatter::HTMLFormatter,
     Coveralls::SimpleCov::Formatter
 ]
@@ -21,5 +11,16 @@ SimpleCov.start do
   add_filter 'test/'
   add_filter 'db/'
 end
+
+require 'minitest/reporters'
+MiniTest::Reporters.use! [MiniTest::Reporters::DefaultReporter.new,
+                          MiniTest::Reporters::JUnitReporter.new]
+
+
+require 'minitest/autorun'
+require 'minitest/benchmark'
+require 'active_record'
+require 'rails'
+require 'index_tree'
 
 Dir["test/*_test.rb"].each { |f| require f[5..-4] }
