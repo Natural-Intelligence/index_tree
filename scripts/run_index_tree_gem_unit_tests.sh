7addc21aeb95a814e64d491fafa9eefc3b74de77
Sergei <Olshanetski@Sergeis-MacBook-Pro.local>
2015-07-20 12:01:41 +0300

    added path param

--- scripts/run_index_tree_gem_unit_tests.sh
+++ scripts/run_index_tree_gem_unit_tests.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+#get path param
+path=$1
+
+#select ruby version
 source /usr/local/rvm/scripts/rvm
 rvm use 2.1.2
-cd /var/jenkins_home/jobs/index_tree_gem_unit_tests/workspace/
+
+cd $path
 
 #run unit tests
 bundle install
