241d3c534e90cbd8b2ea3e86e4611a3022bd91e3
Sergei Olshanetski <23seriy@gmail.com>
2015-07-22 10:20:40 +0300

    added logs and comments

--- scripts/run_index_tree_gem_unit_tests.sh
+++ scripts/run_index_tree_gem_unit_tests.sh
@@ -2,13 +2,14 @@
 
 #get path param
 path=$1
+echo path: $path
 
 #select ruby version
 source /usr/local/rvm/scripts/rvm
 rvm use 2.1.2
-
 cd $path
 
+echo "###########################"
 #run unit tests
 bundle install
 bundle exec rake test
